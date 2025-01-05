25876cb7a50b77b6498745290e2e1fa751bd5d93
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-05 20:23:02 +0200

    push

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    content: 'Nextra Docs Template',
+    content: 'Replyke Documentation',
   },
 }
 
