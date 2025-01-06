cd7531937083dcf8d97c9251d9d538dfdc93cc0e
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-06 10:38:08 +0200

    update contact

--- pages/_meta.tsx
+++ pages/_meta.tsx
@@ -9,14 +9,12 @@ export default {
   relationships: "Relationships",
   "app-notifications": "App notifications",
   reports: "Reports",
-  about: {
-    title: "About",
-    type: "page",
-  },
+  // about: {
+  //   title: "About",
+  //   type: "page",
+  // },
   contact: {
     title: "Contact ↗",
     type: "page",
-    href: "https://twitter.com/shuding_",
-    newWindow: true,
   },
 };
