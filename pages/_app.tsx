fdbd91a4b56c99b8495d3d66d2d7aa595628f335
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-18 20:50:32 +0200

    update docs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
