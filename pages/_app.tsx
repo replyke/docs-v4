6cdd75b8d9c2e88fa5db82dcde85da2194dcefad
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-22 13:39:45 +0200

    nothing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
