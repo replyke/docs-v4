73b772d4195bf646ca2a4edca5aa0fbb0e0405bd
Yanay Zabary <yanay.tsabary@gmail.com>
2025-02-12 13:47:53 +0200

    added comment end points

--- pages/api-endpoints/comments/_meta.tsx
+++ pages/api-endpoints/comments/_meta.tsx
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  "create-comment": "Create comment",
+  "delete-comment": "Delete comment",
+  "downvote-comment": "Downvote comment",
+  "fetch-comments": "Fetch comments",
+  "fetch-single-comment": "Fetch single comment",
+  "remove-comment-downvote": "Remove comment downvote",
+  "remove-comment-upvote": "Remove comment upvote",
+  "update-comment": "Update comment",
+  "upvote-comment": "Upvote comment",
+};
